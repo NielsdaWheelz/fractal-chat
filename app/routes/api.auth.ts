981d3246361691f4f48a925237a4d1eae4998616
Niels-Erik <nielseirknandal@gmail.com>
2025-10-03 16:01:56 -0400

    server-auth

--- app/routes/api.auth.ts
+++ app/routes/api.auth.ts
@@ -1,10 +1,11 @@
-import { auth } from '../utils/auth.server'
 import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node"
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  const { auth } = await import('../utils/auth.server')
   return auth.handler(request)
 }
 
 export async function action({ request }: ActionFunctionArgs) {
+  const { auth } = await import('../utils/auth.server')
   return auth.handler(request)
 }
