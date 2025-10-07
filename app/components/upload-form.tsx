4fe48a79eba0385230bdc774a9c66c95c2e5e913
Jahnik <jahniksuriya@gmail.com>
2025-10-07 13:04:48 -0400

    removed extra package and added basic styling to upload form

--- app/components/upload-form.tsx
+++ app/components/upload-form.tsx
@@ -4,13 +4,13 @@ import { Form } from "react-router";
 export default function UploadForm() {
     return (
         <Form method="post" encType="multipart/form-data" action="/api/upload">
-            <input
+            <input className="text-xs py-2 pl-4 pr-2"
                 type="file"
                 name="file"
                 accept=".pdf,application/pdf,.epub,application/epub+zip"
                 required
             />
-            <button type="submit">upload</button>
+            <button className="text-xs py-2 pl-4 pr-2" type="submit">Upload</button>
         </Form>
     );
 }
