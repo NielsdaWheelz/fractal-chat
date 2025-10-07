d19055960588a71bb193c40c094a54c236b7f7b0
Jahnik <jahniksuriya@gmail.com>
2025-10-07 11:31:05 -0400

    potentially working Epub parse, need to fix db call

--- app/routes/api.upload.ts
+++ app/routes/api.upload.ts
@@ -2,13 +2,16 @@ import type { ActionFunctionArgs } from "react-router";
 import { fileTypeFromBuffer } from 'file-type'
 import { join } from "path";
 import { writeFile, unlink } from "fs/promises";
-import Epub from 'epub2'
+import { EPub } from 'epub2'
 import { tmpdir } from "os";
 import { saveDocument } from "~/index.server";
+import { requireUser } from "~/utils/auth.server";
 
 
 export async function action({ request }: ActionFunctionArgs) {
     const form = await request.formData();
+    const userId = await requireUser(request)
+
     const file = form.get('file') as File | null;
     if (!file) throw new Response("missing file", { status: 400 });
     if (file.size > 5 * 1024 * 1024) throw new Response("file too large", { status: 413 });
@@ -19,14 +22,26 @@ export async function action({ request }: ActionFunctionArgs) {
     await writeFile(tmpPath, buffer);
 
     try {
-        const book = await Epub.createAsync(tmpPath);
+        const book = await EPub.createAsync(tmpPath);
 
         const htmlChapters = await Promise.all(
             book.flow.map(ch => book.getChapterRawAsync(ch.id))
         )
 
+        // const documentId = crypto.randomUUID()
+        // const document = {
+        //   id: documentId,
+        //   userId: userId,
+        //   url: url,
+        //   title: "The Tempest",
+        //   content: article.content ?? "",
+        //   textContent: article.textContent,
+        //   publishedTime: article.publishedTime ?? null,
+        // }
+        // await saveDocument(document)
+
         const html = htmlChapters.join('\n')
-        saveDocument(html)
+        saveDocument({ html })
     } finally {
         await unlink(tmpPath).catch(() => { });
     }
