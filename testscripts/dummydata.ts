f3d89494812f770dc0ccbacb5c5942134e63d05d
kudzuweb <144738515+kudzuweb@users.noreply.github.com>
2025-10-11 10:32:21 -0400

    dummy group data

--- testscripts/dummydata.ts
+++ testscripts/dummydata.ts
@@ -14,3 +14,31 @@ export const users = [
 ]
 
 export default users
+
+const group1 = [{ "id": 1, "name": "Myrlene Danels", "email": "mdanels0@walmart.com", "emailVerified": false, "image": "https://robohash.org/isteatquesed.png?size=50x50&set=set1", "createdAt": "10/26/2024", "updatedAt": "4/29/2025" },
+{ "id": 2, "name": "Jermayne Mac Giany", "email": "jmac1@clickbank.net", "emailVerified": true, "image": "https://robohash.org/itaquedebitisquis.png?size=50x50&set=set1", "createdAt": "1/27/2025", "updatedAt": "12/9/2024" },
+{ "id": 3, "name": "Lewes Kinleyside", "email": "lkinleyside2@cbslocal.com", "emailVerified": true, "image": "https://robohash.org/pariatureumin.png?size=50x50&set=set1", "createdAt": "8/5/2025", "updatedAt": "4/4/2025" },
+{ "id": 4, "name": "Tanny Stainson", "email": "tstainson3@4shared.com", "emailVerified": false, "image": "https://robohash.org/reprehenderitexcepturiquaerat.png?size=50x50&set=set1", "createdAt": "2/7/2025", "updatedAt": "9/15/2025" }]
+
+const group2 = [{ "id": 1, "name": "Cleopatra Denes", "email": "cdenes0@networkadvertising.org", "emailVerified": true, "image": "https://robohash.org/nequequivoluptatibus.png?size=50x50&set=set1", "createdAt": "5/21/2025", "updatedAt": "7/22/2025" },
+{ "id": 2, "name": "Myrilla Bazoche", "email": "mbazoche1@un.org", "emailVerified": false, "image": "https://robohash.org/etveronisi.png?size=50x50&set=set1", "createdAt": "11/29/2024", "updatedAt": "6/6/2025" },
+{ "id": 3, "name": "Elberta Cooke", "email": "ecooke2@live.com", "emailVerified": false, "image": "https://robohash.org/sequirerumdoloremque.png?size=50x50&set=set1", "createdAt": "1/27/2025", "updatedAt": "6/13/2025" },
+{ "id": 4, "name": "Tomasina Zoellner", "email": "tzoellner3@so-net.ne.jp", "emailVerified": false, "image": "https://robohash.org/utistedolorem.png?size=50x50&set=set1", "createdAt": "3/3/2025", "updatedAt": "7/22/2025" },
+{ "id": 5, "name": "Guinevere Ronchi", "email": "gronchi4@g.co", "emailVerified": true, "image": "https://robohash.org/itaqueetut.png?size=50x50&set=set1", "createdAt": "1/13/2025", "updatedAt": "8/5/2025" },
+{ "id": 6, "name": "Wileen Sterry", "email": "wsterry5@canalblog.com", "emailVerified": true, "image": "https://robohash.org/ipsaoditaut.png?size=50x50&set=set1", "createdAt": "8/23/2025", "updatedAt": "10/23/2024" }]
+
+const group3 = [{ "id": 1, "name": "Kimberlee Hallbord", "email": "khallbord0@lulu.com", "emailVerified": false, "image": "https://robohash.org/fugiatperspiciatisipsa.png?size=50x50&set=set1", "createdAt": "5/24/2025", "updatedAt": "10/20/2024" },
+{ "id": 2, "name": "Geoffrey Klimkin", "email": "gklimkin1@comcast.net", "emailVerified": false, "image": "https://robohash.org/rerumaliquidqui.png?size=50x50&set=set1", "createdAt": "5/9/2025", "updatedAt": "8/12/2025" }]
+
+const group4 = [{ "id": 1, "name": "Malynda Hyder", "email": "mhyder0@webeden.co.uk", "emailVerified": false, "image": "https://robohash.org/sequiveroassumenda.png?size=50x50&set=set1", "createdAt": "11/9/2024", "updatedAt": "4/20/2025" },
+{ "id": 2, "name": "Lionello Franzke", "email": "lfranzke1@amazon.de", "emailVerified": false, "image": "https://robohash.org/sedrationevero.png?size=50x50&set=set1", "createdAt": "1/9/2025", "updatedAt": "12/31/2024" },
+{ "id": 3, "name": "Zabrina Patrono", "email": "zpatrono2@photobucket.com", "emailVerified": false, "image": "https://robohash.org/autemnecessitatibusquas.png?size=50x50&set=set1", "createdAt": "1/17/2025", "updatedAt": "4/7/2025" },
+{ "id": 4, "name": "Ashley Broszkiewicz", "email": "abroszkiewicz3@wikia.com", "emailVerified": true, "image": "https://robohash.org/doloremquedelectussint.png?size=50x50&set=set1", "createdAt": "2/20/2025", "updatedAt": "4/12/2025" },
+{ "id": 5, "name": "Randie Golds", "email": "rgolds4@infoseek.co.jp", "emailVerified": false, "image": "https://robohash.org/nemodelectusut.png?size=50x50&set=set1", "createdAt": "4/30/2025", "updatedAt": "7/15/2025" },
+{ "id": 6, "name": "Mauricio Guard", "email": "mguard5@uol.com.br", "emailVerified": false, "image": "https://robohash.org/consequunturmodinesciunt.png?size=50x50&set=set1", "createdAt": "9/6/2025", "updatedAt": "11/1/2024" }]
+
+const group5 = [{ "id": 1, "name": "Darcy Beaudry", "email": "dbeaudry0@barnesandnoble.com", "emailVerified": false, "image": "https://robohash.org/etnonperferendis.png?size=50x50&set=set1", "createdAt": "12/3/2024", "updatedAt": "8/13/2025" },
+{ "id": 2, "name": "Hilliary Kenion", "email": "hkenion1@loc.gov", "emailVerified": false, "image": "https://robohash.org/oditipsavoluptate.png?size=50x50&set=set1", "createdAt": "10/16/2024", "updatedAt": "1/7/2025" },
+{ "id": 3, "name": "Renaud Erlam", "email": "rerlam2@ucla.edu", "emailVerified": false, "image": "https://robohash.org/temporibusblanditiisfugiat.png?size=50x50&set=set1", "createdAt": "7/15/2025", "updatedAt": "2/18/2025" },
+{ "id": 4, "name": "Melicent Groarty", "email": "mgroarty3@go.com", "emailVerified": false, "image": "https://robohash.org/etnesciuntdistinctio.png?size=50x50&set=set1", "createdAt": "11/11/2024", "updatedAt": "4/9/2025" },
+{ "id": 5, "name": "Olly Kundert", "email": "okundert4@slate.com", "emailVerified": false, "image": "https://robohash.org/maioresillumrem.png?size=50x50&set=set1", "createdAt": "4/25/2025", "updatedAt": "7/7/2025" }]
